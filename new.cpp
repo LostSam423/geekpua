4aad38b825b3d557abf1d67ebd659c4cd34dace6
LostSam423 <sambit_behera@Sambits-Macbook-Pro.local>
2019-09-28 10:41:26 +0530

    Apun d ko bhi cout karela hai idhar

--- new.cpp
+++ new.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
 	int pre[1000];
 	pre[1000]=29;
-	 pre[1001]=282;
+	pre[1001]=282;
 	double d= pre[-4];
-	cout<<pre[1001]<<endl<<pre[1000];
+	cout<<pre[1001]<<endl<<pre[1000]<<endl<<d;
 }
